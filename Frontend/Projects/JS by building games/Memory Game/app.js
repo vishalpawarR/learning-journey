833fb0339266069de65da0df0e2119eefcf80613
Vishal Pawar <vishalpawar425@gmail.com>
2021-03-08 09:27:13 +0530

    game js basics array

--- Frontend/Projects/JS by building games/Memory Game/app.js
+++ Frontend/Projects/JS by building games/Memory Game/app.js
@@ -1,3 +1,52 @@
 document.addEventListener('DOMContentLoaded', () => {
     // card options
+    const cardArray = [{
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'images/cheeseburger.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+        },
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'images/cheeseburger.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+        }
+    ]
 })
